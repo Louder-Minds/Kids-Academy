364d4d589824e2c633d9b55ca68006b9cda1c4c2
rubennijhuis <contact@rubenswebsite.com>
2020-04-22 20:44:55 +0200

    CONFIG

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,21 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        options: {
+          enableIdentityWidget: true,
+        },
+      },
+    },
   ],
 }
---
+++ src/pages/over-ons.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+
+const index = () => (
+  <Layout>{/* <Title>Over ons</Title>
+    <AboutUs />
+    <hr /> */}</Layout>
+)
+
+export default index
