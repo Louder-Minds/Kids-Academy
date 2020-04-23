1556c934f8e5353675a3c7ae322e18085f6f4e98
rubennijhuis <contact@rubenswebsite.com>
2020-04-23 10:17:57 +0200

    cms testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  siteMetadata: {
-    title: `KidsAcademy`,
-    description: `...`,
-    author: `@grubennijhuis`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `KidsAcademy`,
+        description: `...`,
+        author: `@grubennijhuis`,
     },
-    {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-        options: {
-          enableIdentityWidget: true,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-styled-components`,
+        `gatsby-plugin-netlify-cms`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images/`,
+            },
         },
-      },
-    },
-  ],
-}
+        {
+            resolve: `gatsby-plugin-netlify-cms`,
+            options: {
+                options: {
+                    enableIdentityWidget: true,
+                },
+            },
+        },
+    ],
+};
