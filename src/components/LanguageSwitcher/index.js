a126627b0d08ddaf1266eeadd7085ded3ef3f3e3
almostperfect <contact@rubenswebsite.com>
2020-05-24 16:17:54 +0200

    shortened navbar padding

--- src/components/LanguageSwitcher/index.js
+++ src/components/LanguageSwitcher/index.js
@@ -7,37 +7,33 @@ const languageName = {
 };
 
 const Language = () => (
-    <div>
-        <IntlContextConsumer>
-            {({ languages, language: currentLocale }) => (
-                <div
-                    style={{
-                        backgroundColor: 'rgba(0,0,0,0.1)',
-                        padding: '5px 0',
-                    }}
-                >
-                    {languages.map((language) => (
-                        <a
-                            key={language}
-                            onClick={() => changeLocale(language)}
-                            style={{
-                                backgroundColor:
-                                    currentLocale === language
-                                        ? `#14b09c`
-                                        : `none`,
-                                margin: 10,
-                                cursor: `pointer`,
-                                padding: '5px',
-                                margin: '0',
-                            }}
-                        >
-                            {languageName[language]}
-                        </a>
-                    ))}
-                </div>
-            )}
-        </IntlContextConsumer>
-    </div>
+    <IntlContextConsumer>
+        {({ languages, language: currentLocale }) => (
+            <div
+                style={{
+                    backgroundColor: 'rgba(0,0,0,0.1)',
+                    padding: '5px 0',
+                }}
+            >
+                {languages.map((language) => (
+                    <a
+                        key={language}
+                        onClick={() => changeLocale(language)}
+                        style={{
+                            backgroundColor:
+                                currentLocale === language ? `#14b09c` : `none`,
+                            margin: 10,
+                            cursor: `pointer`,
+                            padding: '5px',
+                            margin: '0',
+                        }}
+                    >
+                        {languageName[language]}
+                    </a>
+                ))}
+            </div>
+        )}
+    </IntlContextConsumer>
 );
 
 export default Language;
